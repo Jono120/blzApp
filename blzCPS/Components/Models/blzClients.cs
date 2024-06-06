7f8a5eee0477c11b96f063d41534da4b3d5ddfae
Jonathan <25333421+Jono120@users.noreply.github.com>
2024-06-06 13:02:24 +1200

    Null Reference Fix

--- blzCPS/Components/Models/blzClients.cs
+++ blzCPS/Components/Models/blzClients.cs
@@ -5,12 +5,12 @@ namespace blzCPS.Components.Models
     public class blzClients
     {
         public int ClientId { get; set; }
-        public string? StaffMemberName { get; set; } // Nullable string
+        public string StaffMemberName { get; set; } // Nullable string
         public string ClientName { get; set; } // Nullable string
-        public string? ClientPhoneNumber { get; set; } // Nullable string
-        public string? ClientEmail { get; set; } // Nullable string
-        public string? ClientLocation { get; set; } // Nullable string
-        public bool? HasBeenContacted { get; set; } // Nullable boolean
-        public string? ClientNotes { get; set; } // Nullable string
+        public string ClientPhoneNumber { get; set; } // Nullable string
+        public string ClientEmail { get; set; } // Nullable string
+        public string ClientLocation { get; set; } // Nullable string
+        public bool HasBeenContacted { get; set; } // Nullable boolean
+        public string ClientNotes { get; set; } // Nullable string
     }
 }
